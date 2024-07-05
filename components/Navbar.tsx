227fbbc764e87499feb5f6bf7378e1822fca7292
omotayobolu <omotayodaniel10@gmail.com>
2024-07-05 18:49:59 +0100

    navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <header className="mx-[5.7%]">
+      <div className="py-[2.1875rem] flex flex-row justify-between">
+        <div className="flex flex-row items-center space-x-[8.75rem]">
+          <h2 className="uppercase font-elmessiri">timbu</h2>
+          <div className="flex flex-row items-center space-x-12">
+            <Link href="">Home</Link>
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? "text-primary-orange font-medium" : ""
+              }`}
+            >
+              Products
+            </Link>
+            <Link href="">Contact Us</Link>
+          </div>
+        </div>
+        <div className="flex flex-row items-center space-x-8">
+          <IoSearchSharp className="w-6 h-6 text-primary-black cursor-pointer" />
+          <FaRegHeart className="w-6 h-6 text-primary-black cursor-pointer" />
+          <HiOutlineShoppingCart className="w-6 h-6 text-primary-black cursor-pointer" />
+          <Image
+            src="/assets/nav-icon.png"
+            alt="nav icon"
+            width={42}
+            height={42}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary-black": "#121212",
+        "secondary-black": "#4F4F4F",
+        "primary-orange": "#B05405",
+      },
+      fontFamily: {
+        elmessiri: ["var(--font-elmessiri)"],
+        aeonik: ["var(--font-aeonik)"],
       },
     },
   },
